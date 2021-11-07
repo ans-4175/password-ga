3a7b213710f9ef2a127da1e99a59d44df3f0941d
Ahmad Anshorimuslim Syuhada <ans4175@gmail.com>
2021-11-07 22:47:01 +0700

    Feat/wired (#1)
    * init
    
    * fix(index): change title
    
    * feat(App): add copy to clipboard
    
    * feat(App): re-generate password
    
    * feat(App): libs password-ga

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
+import { WiredButton, WiredCard, WiredInput } from "wired-elements-react";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import generatePasswords from "./libs/password-ga";
+
+import "./App.css";
 
 function App() {
+  let textInput = useRef({});
+  const [password, setPassword] = useState([]);
+  const [passwordLoaded, setPasswordLoaded] = useState(false);
+  const [copied, setCopied] = useState(false);
+
+  const reGenerate = async () => {
+    const res = generatePasswords({});
+    setPasswordLoaded(false);
+    setCopied(false);
+    if (res.length) {
+      setPassword(res[0]);
+      setPasswordLoaded(true);
+      textInput.current.value = res[0];
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      await reGenerate();
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <WiredCard elevation={3}>
+        <h1>Password Generator</h1>
+        {!passwordLoaded ? 
+        (<p>Loading</p>) : (
+          <>
+          <section>
+          <WiredInput
+            disabled={true}
+            placeholder="password here"
+            value={password}
+            ref={textInput}
+          />
+          <WiredButton elevation={2} onClick={() => reGenerate()}>
+            Re-Gen
+          </WiredButton>
+          </section>
+          <section>
+            <CopyToClipboard text={password}
+              onCopy={() => setCopied(true)}>
+              <WiredButton className="btn-copy" elevation={2}>
+                Copy
+              </WiredButton>
+            </CopyToClipboard>
+            {copied && <div className="copied">copied</div>}
+          </section>
+          </>
+        )}
+      </WiredCard>
+    </main>
   );
 }
 
