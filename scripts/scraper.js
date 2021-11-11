24afadf722984374c8fde24f0667f72d2dc9417d
Try Ajitiono <ballinst@gmail.com>
2021-11-11 10:51:53 +0700

    add new dictionary
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- scripts/scraper.js
+++ scripts/scraper.js
@@ -3,8 +3,14 @@ const cheerio = require('cheerio');
 const fs = require('fs/promises');
 const path = require('path');
 
-const NOUNS_BASE_URL = 'https://kbbi.kata.web.id/kelas-kata/kata-benda';
-const ADJECTIVES_BASE_URL = 'https://kbbi.kata.web.id/kelas-kata/kata-sifat';
+const NOUNS_BASE_URL =
+  'http://kateglo.com?op=1&phrase=&lex=n&type=r&src=&mod=dictionary&srch=Cari';
+const ADJECTIVES_BASE_URL =
+  'http://kateglo.com?&op=1&phrase=&lex=adj&type=r&src=&mod=dictionary&srch=Cari';
+
+// Shorter word length is not that "strong".
+// Perhaps it's a good idea to only include words that are at least a certain length.
+const MINIMUM_WORD_LENGTH = 5;
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -19,13 +25,15 @@ const ADJECTIVES_BASE_URL = 'https://kbbi.kata.web.id/kelas-kata/kata-sifat';
   const nounPageNumbers = getFetchedPageNumbers(lastNounPageNumber);
 
   for (const pageNumber of nounPageNumbers) {
-    console.info(`[info] scraping noun page ${pageNumber}...`);
+    console.info(`[info] scraping nouns in page ${pageNumber}...`);
 
-    await page.goto(`${NOUNS_BASE_URL}/page/${pageNumber}`);
+    await page.goto(`${NOUNS_BASE_URL}&p=${pageNumber}`);
     const pageNouns = await getWordsFromPage(page);
 
     for (const word of pageNouns) {
-      nouns.add(word);
+      if (word.length >= MINIMUM_WORD_LENGTH) {
+        nouns.add(word);
+      }
     }
   }
 
@@ -38,21 +46,34 @@ const ADJECTIVES_BASE_URL = 'https://kbbi.kata.web.id/kelas-kata/kata-sifat';
   const adjectivePageNumbers = getFetchedPageNumbers(lastAdjectivePageNumber);
 
   for (const pageNumber of adjectivePageNumbers) {
-    console.info(`[info] scraping adjective page ${pageNumber}...`);
+    console.info(`[info] scraping adjectives in page ${pageNumber}...`);
 
-    await page.goto(`${ADJECTIVES_BASE_URL}/page/${pageNumber}`);
+    await page.goto(`${ADJECTIVES_BASE_URL}&p=${pageNumber}`);
     const pageAdjectives = await getWordsFromPage(page);
 
     for (const word of pageAdjectives) {
-      adjectives.add(word);
+      if (word.length >= MINIMUM_WORD_LENGTH) {
+        adjectives.add(word);
+      }
     }
   }
 
+  console.info(
+    `Total scraped words: ${nouns.size} nouns, ${adjectives.size} adjectives.`
+  );
+
   // Save to local JSON file.
-  await fs.writeFile(path.join(__dirname, '../src/words.json'), {
-    nouns: Array.from(nouns),
-    adjectives: Array.from(adjectives)
-  });
+  await fs.writeFile(
+    path.join(__dirname, '../src/words.json'),
+    JSON.stringify(
+      {
+        nouns: Array.from(nouns).sort(),
+        adjectives: Array.from(adjectives).sort()
+      },
+      null,
+      2
+    )
+  );
   await browser.close();
 })();
 
@@ -60,10 +81,10 @@ const ADJECTIVES_BASE_URL = 'https://kbbi.kata.web.id/kelas-kata/kata-sifat';
 async function getWordsFromPage(page) {
   const data = await page.evaluate(() => document.querySelector('*').outerHTML);
 
-  const $ = cheerio.load(data);
+  const selector = cheerio.load(data);
   const words = [];
 
-  $('dl > dt > a').each((_idx, element) => {
+  selector('dl > dt > a').each((_idx, element) => {
     const word = cheerio.load(element.children[0]).text();
     // Clear parentheses (if any).
     // TODO(imballinst): check if we want to remove symbols such as hyphen as well.
@@ -75,17 +96,22 @@ async function getWordsFromPage(page) {
 
 async function getNumberOfLastPage(page) {
   const data = await page.evaluate(() => document.querySelector('*').outerHTML);
-  const $ = cheerio.load(data);
+  const selector = cheerio.load(data);
 
-  const lastPage = $('div.pagination > a.page-numbers:not(.next)')
-    .last()
-    .text();
+  const paginationSelector = cheerio.load(
+    selector('ul.pagination').first().html()
+  );
+  const pagingElement = paginationSelector('li > a').last();
+  // The last page URL is something like this: ./?&op=1&phrase=&lex=n&type=r&src=&mod=dictionary&srch=Cari&p=403.
+  // We want to take the `p` query parameter only.
+  const lastPage = pagingElement.attr('href').split('&p=')[1];
+
   return Number(lastPage.replace(',', ''));
 }
 
 // At the moment, we settle for ~500 words.
 // Reference: https://github.com/ans-4175/password-ga/issues/9#issuecomment-964749676.
-const NUMBER_OF_FETCHED_PAGES = 5;
+const NUMBER_OF_FETCHED_PAGES = 50;
 
 /**
  * Get the page numbers that will to be fetched.
